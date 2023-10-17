7d659f1dbfcd25f34e2b135674a0ddafcf610e11
Naaagy <145921671+Naaagy@users.noreply.github.com>
2023-10-17 09:27:38 +0200

    Add files via upload

---
+++ javito.js
@@ -0,0 +1,5 @@
+document.querySelector('.container').style.backgroundColor = '#12406A';
+document.querySelector('.box1').style.backgroundColor = '#EB8F90';
+document.querySelector('.box2').style.backgroundColor = '#FFB471';
+document.querySelector('.box3').style.backgroundColor = '#ADBED2';
+document.querySelector('.body').style.backgroundColor = '#CCFFFF';
